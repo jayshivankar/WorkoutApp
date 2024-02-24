079158727cea267e2ca3452496d8afba989d001a
Jay <jp6631@srmist.edu.in>
2024-02-24 22:58:44 +0530

    creating opening page

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+p
+@app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt,0/5/05ee8408ed330e9feba8796884c70e71cd04bbf7
+D
+app/build.gradle.kts,d/2/d2a4383452aad84a18429a5844d31330a012a9e6
+@
+build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
+t
+Dapp/src/main/java/com/example/a7minuteworkoutapp/ExerciseActivity.kt,6/e/6ebb685044106779026eed57b822dffbaf91c3d8
---
+++ app/src/main/java/com/example/a7minuteworkoutapp/ExerciseActivity.kt
@@ -0,0 +1,27 @@
+package com.example.a7minuteworkoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a7minuteworkoutapp.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private var binding : ActivityExerciseBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+    }
+
+
+}
